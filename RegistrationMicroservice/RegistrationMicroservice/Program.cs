dbe03a7b5e92691026b289809ff8084f3ea84cf2
Badri7171 <96404494+Badri7171@users.noreply.github.com>
2022-08-04 14:58:11 +0530

    Add files via upload

--- RegistrationMicroservice/RegistrationMicroservice/Program.cs
+++ RegistrationMicroservice/RegistrationMicroservice/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Xml;
 
 namespace RegistrationMicroservice
 {
@@ -13,6 +10,10 @@ namespace RegistrationMicroservice
     {
         public static void Main(string[] args)
         {
+            XmlDocument log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead("log4net.config"));
+            var log4netRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+            log4net.Config.XmlConfigurator.Configure(log4netRepository, new FileInfo("log4net.config"));
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -21,6 +22,7 @@ namespace RegistrationMicroservice
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
     }
 }
--- RegistrationMicroservice/RegistrationMicroservice/Startup.cs
+++ RegistrationMicroservice/RegistrationMicroservice/Startup.cs
@@ -2,11 +2,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using RegistrationMicroservice.Database;
@@ -30,10 +30,10 @@ namespace RegistrationMicroservice
             services.AddDbContext<RegistrationContext>(
               options => options.UseSqlServer(Configuration.GetConnectionString("RegistrationDb")));
 
-          
+
             services.AddControllers();
             services.AddTransient<IRegRepository, RegRepository>();
-            
+
             services.AddAutoMapper(typeof(Startup));
             services.AddCors(option =>
             {
@@ -45,13 +45,34 @@ namespace RegistrationMicroservice
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RegistrationMicroservice", Version = "v1" });
+
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                }).AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration["JWT:ValidAudience"],
+                        ValidAudience = Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Scheme"]))
+                    };
+                });
             });
-        
         }
+            
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory logger)
         {
+            logger.AddLog4Net();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -60,8 +81,11 @@ namespace RegistrationMicroservice
             }
 
             app.UseRouting();
+
             app.UseCors();
+
             app.UseAuthentication();
+
             app.UseAuthorization();
             
            
